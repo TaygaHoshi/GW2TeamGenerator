c1411c458aeccc365f2b449221755393e6bc4a39
TaygaHoshi <85090948+TaygaHoshi@users.noreply.github.com>
2021-10-08 15:57:10 +0300

    Fix #11

--- GW2TG/Start.py
+++ GW2TG/Start.py
@@ -1,13 +1,27 @@
 import tkinter as tk
+from tkinter import StringVar, filedialog as fd
 from GW2TG import *
 from utility import *
 
-def normalize_strings(input_filepath, output_filepath, style, reroll_count):
-    # input_filepath
-    input_filepath = input_filepath
+def select_file(buffer:StringVar, button:tk.Button):
+    filetypes = (
+        ("Text files", ".txt"),
+        ("Tab seperated values", ".tsv"),
+        ('All files', '*.*')
+    )
 
-    # output_filepath
-    output_filepath = output_filepath
+    filename = fd.askopenfilename(
+        title='Open a file',
+        initialdir='/',
+        filetypes=filetypes)
+
+    buffer.set(filename)
+
+    button["text"] = filename.split("/")[-1]
+    button.update()
+
+
+def normalize_strings(style, reroll_count):
 
     # style
     style = style.replace(" ", "").lower()
@@ -24,19 +38,17 @@ def normalize_strings(input_filepath, output_filepath, style, reroll_count):
     else:
         reroll_count = int(reroll_count)
 
-    return (input_filepath, output_filepath, style, reroll_count)
+    return (style, reroll_count)
 
-def generate(input_filepath_entry, output_filepath_entry, style_entry, reroll_count_entry):
+def generate(input_filepath, output_filepath, style_entry, reroll_count_entry):
 
     # get strings from the GUI
-    input_filepath = input_filepath_entry.get()
-    output_filepath = output_filepath_entry.get()
     style = style_entry.get()
     reroll_count = reroll_count_entry.get()
     
     # normalize the input strings
 
-    input_filepath, output_filepath, style, reroll_count = normalize_strings(input_filepath, output_filepath, style, reroll_count)
+    style, reroll_count = normalize_strings(style, reroll_count)
     
 
     # read player file, generate result and save output
@@ -53,13 +65,17 @@ if __name__=="__main__":
     root.maxsize(500, 300)
     root.iconbitmap(".\icon.ico")
 
+    # temporary variables
+    input_filepath = StringVar("")
+    output_filepath = StringVar("")
+
     # menu elements
     io_frame = tk.Frame(root)
     io_explanation = tk.Label(io_frame, text="Leave a spot empty to use its default value.")
-    input_label = tk.Label(io_frame, text="Input File (Full path)")
-    input_entry = tk.Entry(io_frame)
+    input_label = tk.Label(io_frame, text="Input File")
+    input_button = tk.Button(io_frame, text="Choose Input File", command=lambda: select_file(input_filepath, input_button))
     output_label = tk.Label(io_frame, text="Output File (Full path)")
-    output_entry = tk.Entry(io_frame)
+    output_button = tk.Button(io_frame, text="Choose Output File", command=lambda: select_file(output_filepath, output_button))
 
     options_frame = tk.Frame(root)
     style_label = tk.Label(options_frame, text="Style")
@@ -69,16 +85,16 @@ if __name__=="__main__":
 
     # button to call the generate() function
     generate_button = tk.Button(root, text="Generate", command=lambda: 
-                                generate(input_entry, output_entry, style_entry, reroll_entry))
+                                generate(input_filepath.get(), output_filepath.get(), style_entry, reroll_entry))
 
     # placement of GUI elements
     ## input/output
     io_frame.pack()
     io_explanation.pack()
     input_label.pack()
-    input_entry.pack()
+    input_button.pack(expand=True)
     output_label.pack()
-    output_entry.pack()
+    output_button.pack(expand=True)
 
     ## options
     options_frame.pack()
