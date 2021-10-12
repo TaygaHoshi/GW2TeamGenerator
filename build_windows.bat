724c319587494c08dd5df8d1d51cc572c2c3d3db
TaygaHoshi <85090948+TaygaHoshi@users.noreply.github.com>
2021-10-12 12:28:44 +0300

    Update build_windows.bat

--- build_windows.bat
+++ build_windows.bat
@@ -1,4 +1,4 @@
-pyinstaller --clean -c -F --icon=".\GW2TG\icon.ico" ".\GW2TG\Start.py" -n "GW2TG.exe"
-COPY ".\GW2TG\icon.ico" ".\dist\icon.ico"
-COPY ".\GW2TG\players.tsv" ".\dist\players.tsv"
+pyinstaller --noconsole --clean --icon=".\GW2TG\icon.ico" ".\GW2TG\Start.py" -n "GW2TG.exe"
+COPY ".\GW2TG\icon.ico" ".\dist\GW2TG.exe\icon.ico"
+COPY ".\GW2TG\players.tsv" ".\dist\GW2TG.exe\players.tsv"
 pause
